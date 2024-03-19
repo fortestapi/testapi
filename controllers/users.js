982c8b334223a4de718ae48329ddfc7fe4d07582
BABUTSIDZ3 <babutsidzegia7@gmail.com>
2024-03-19 14:22:36 +0400

    t

--- controllers/users.js
+++ controllers/users.js
@@ -5,7 +5,9 @@ const usersRouter = express.Router();
 
 usersRouter.get("/", async (req, res) => {
   try {
-   res.send('users')
+    const sql_query = `SELECT * FROM users`;
+    const results = await queryDatabase(sql_query);
+    res.send(results);
   } catch (err) {
     res.send(err.message);
   }
